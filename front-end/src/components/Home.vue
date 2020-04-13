68f29f14f92d7a2d6c82f28ed5cfb7c0f324d24a
Joshua Torres Berrocal <joshtbcr@gmail.com>
2020-04-13 12:59:37 -0600

    Agregando prueba en Home para conectar con WebApi

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <p>{{ msg }}</p>
+    <p @click="cargar">{{ msg }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+
 export default {
   name: 'Home',
   data() {
@@ -12,5 +15,18 @@ export default {
       msg: 'Hello!',
     };
   },
+  methods:{
+    cargar() {
+      const path = 'http://localhost:5000/ordenes/all';
+      axios.get(path)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    }
+  }
 };
 </script>
